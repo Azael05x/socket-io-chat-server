de2c4cfd65939d9e9ff32c8c663a135b6dca4643
Aigars Cibulskis <azael05x@gmail.com>
2018-10-19 12:27:36 +0300

    Add chatroom consts

---
+++ src/config/consts.ts
@@ -0,0 +1,4 @@
+export const KICK_SILENT_MIN = 5;
+export const KICK_SILENT_MESSAGE = (username: string) => `${username} was disconnected due to inactivity`;
+
+export const DISCONNECTED_MESSAGE = (username: string) => `${username} left the chat, connection lost`;
