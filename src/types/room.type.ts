8553af6199d6fc999f6ec2cc09b2aebbeadbf70d
Aigars Cibulskis <azael05x@gmail.com>
2018-10-21 11:31:48 +0300

    Add Room Type

---
+++ src/types/room.type.ts
@@ -0,0 +1,20 @@
+import { Socket } from "socket.io";
+
+export interface RoomUser {
+  socket: Socket;
+  nickname: string;
+  timeoutHandle: number;
+}
+
+export interface RoomMessage {
+  nickname: string;
+  text: string;
+  timestamp: number;
+  isAnnouncment: false;
+}
+
+export interface RoomAnnouncment {
+  text: string;
+  timestamp: number;
+  isAnnouncment: true;
+}
